7047659bf2ee01df81a1e6fd5c4ac5a641317c65
amitrm <amit.armondal@gmail.com>
2022-04-03 22:44:23 -0500

    Add app title.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import dash_bootstrap_components as dbc
 # Initiate app
 external_stylesheets = [dbc.themes.LUMEN]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = 'Here it is!'
 server = app.server
 
 # Layout
